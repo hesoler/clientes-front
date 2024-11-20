75073eb38ac88b7253b486e6e84430c170768092
Héctor Soler <hecalana@gmail.com>
2024-11-20 11:39:22 -0500

    Add toasty notifications

--- src/app/clients/form/form.component.ts
+++ src/app/clients/form/form.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core'
 import { FormsModule } from '@angular/forms'
+import { Router } from '@angular/router'
+import Swal from 'sweetalert2'
 import { Client } from '../client'
 import { ClientService } from './../client.service'
-import { Router } from '@angular/router'
-
 @Component({
   selector: 'app-form',
   standalone: true,
@@ -23,9 +23,19 @@ export class FormComponent implements OnInit {
   }
 
   public create (): void {
-    this.clientService.create(this.client).subscribe(
-      (response) => { this.router.navigate(['/clients']) }
-    )
+    this.clientService.create(this.client)
+      .subscribe(
+        (client) => {
+          this.router.navigate(['/clients'])
+          Swal.fire({
+            title: 'New client',
+            text: `Client ${client.name} created successfully!`,
+            icon: 'success',
+            showConfirmButton: false,
+            timer: 2000
+          })
+        }
+      )
   }
 
   ngOnInit () { }
